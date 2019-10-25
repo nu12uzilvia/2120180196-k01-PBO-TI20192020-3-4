6c5aaf7c8f4d0718ce4d6c72556e09aad73a11dc
nuruz zilvia <nu12uzilvi4@gmail.com>
2019-10-25 09:02:47 +0700

    upload tugas kuis 01

---
+++ 2120180196-K01-PBO-TI20192020-3-4-02/src/pkg2120180196/k01/pbo/ti20192020/pkg3/pkg4/pkg02/K01PBOTI201920203402.java
@@ -0,0 +1,47 @@
+package pkg2120180196.k01.pbo.ti20192020.pkg3.pkg4.pkg02;
+public class K01PBOTI201920203402 {
+    public static void main(String[] args) {
+             //instansiasi
+    mahasiswa1 mahasiswa_biodata1 = new mahasiswa1 ();
+    //inisialisasi
+    System.out.println(mahasiswa_biodata1.nama);
+    System.out.println(mahasiswa_biodata1.alamat);
+    System.out.println(mahasiswa_biodata1.nim);
+    //pemanggilan method
+    mahasiswa_biodata1.biodata();
+    
+    System.out.println("========================================================");
+    mahasiswa2 mahasiswa_biodata2 = new mahasiswa2();
+    System.out.println(mahasiswa_biodata2.nama);
+    System.out.println(mahasiswa_biodata2.alamat);
+    System.out.println(mahasiswa_biodata2.nim);
+    //pemanggilan method
+    mahasiswa_biodata2.biodata();
+    
+    System.out.println("========================================================");
+    mahasiswa3 mahasiswa_biodata3 = new mahasiswa3();
+    System.out.println(mahasiswa_biodata3.nama);
+    System.out.println(mahasiswa_biodata3.alamat);
+    System.out.println(mahasiswa_biodata3.nim);
+    //pemanggilan method
+    mahasiswa_biodata3.biodata();
+    
+    System.out.println("========================================================");
+    mahasiswa4 mahasiswa_biodata4 = new mahasiswa4();
+    System.out.println(mahasiswa_biodata4.nama);
+    System.out.println(mahasiswa_biodata4.alamat);
+    System.out.println(mahasiswa_biodata4.nim);
+    //pemanggilan method
+    mahasiswa_biodata4.biodata();
+    
+    System.out.println("========================================================");
+    mahasiswa5 mahasiswa_biodata5 = new mahasiswa5();
+    System.out.println(mahasiswa_biodata5.nama);
+    System.out.println(mahasiswa_biodata5.alamat);
+    System.out.println(mahasiswa_biodata5.nim);
+    //pemanggilan method
+    mahasiswa_biodata5.biodata();
+    
+    }
+    
+}
---
+++ 2120180196-K01-PBO-TI20192020-3-4-02/src/pkg2120180196/k01/pbo/ti20192020/pkg3/pkg4/pkg02/mahasiswa1.java
@@ -0,0 +1,14 @@
+package pkg2120180196.k01.pbo.ti20192020.pkg3.pkg4.pkg02;
+public class mahasiswa1 {
+    String nama = ("nuruz zilviana nur baidah");
+    String alamat = ("JLN.ahmat yani nomer 10");
+    String nim = ("2120180196");
+    void biodata(){
+        System.out.println("biodata mahasiswa                   : ");
+        System.out.println("1. nama mahasiswa                   : " +this.nama);
+        System.out.println("2. alamat mahasiswa                 : " +this.alamat);
+        System.out.println("3. tempat tanggal lahir             : bojonegoro 26 juli 2000");
+        System.out.println("4. hobby                            : klayapan");
+        System.out.println("5. jenis kelamin                    : wanita");
+    }
+}
